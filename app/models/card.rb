de03b9f00f7ce9636983f7cbb3eb72057bddcbd2
ruyielder <ruyielder@gmail.com>
2015-01-01 23:50:04 +0100

    Added validators to Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ActiveRecord::Base
   belongs_to :user
+
+  validates :first, :second, presence: true
+  validates :negative_count, :positive_count, :positive_percent, :numericality => { :greater_than_or_equal_to => 0 }
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Card, :type => :model do
   it 'has a valid factory' do
     expect(build(:card, user: user)).to be_valid
   end
+
+  it 'is invalid without first value' do
+    expect(build(:card, user: user, first: '')).not_to be_valid
+  end
+
+  it 'is invalid without second value' do
+    expect(build(:card, user: user, second: '')).not_to be_valid
+  end
+
+  it 'is invalid with negative negative_count' do
+    expect(build(:card, user: user, negative_count: -1)).not_to be_valid
+  end
+
+  it 'is invalid with negative positive_count' do
+    expect(build(:card, user: user, positive_count: -1)).not_to be_valid
+  end
+
+  it 'is invalid with negative positive_percent' do
+    expect(build(:card, user: user, positive_percent: -1)).not_to be_valid
+  end
 end
