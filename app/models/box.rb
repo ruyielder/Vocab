3d948772505c5fc5db6563d1c63b995fe29896a3
ruyielder <ruyielder@gmail.com>
2015-01-02 15:55:18 +0100

    Added validator to box title

--- app/models/box.rb
+++ app/models/box.rb
@@ -2,4 +2,5 @@ class Box < ActiveRecord::Base
   belongs_to :user
 
   validates :user, :title, presence: true
+  validates_uniqueness_of :title
 end
--- spec/models/box_spec.rb
+++ spec/models/box_spec.rb
@@ -7,11 +7,16 @@ RSpec.describe Box, :type => :model do
     expect(build(:box, user: user)).to be_valid
   end
 
+  it 'is invalid without user' do
+    expect(build(:box)).not_to be_valid
+  end
+
   it 'is invalid without title' do
     expect(build(:box, user: user, title: '')).not_to be_valid
   end
 
-  it 'is invalid without user' do
-    expect(build(:box)).not_to be_valid
+  it 'is invalid with not unique title' do
+    create(:box, user: user, title: 'Verbs')
+    expect(build(:box, user: user, title: 'Verbs')).not_to be_valid
   end
 end
