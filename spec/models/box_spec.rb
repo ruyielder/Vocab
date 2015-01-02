6e9d8511936c199bddc23992ce00bb551833479b
ruyielder <ruyielder@gmail.com>
2015-01-02 15:42:11 +0100

    Created box factory

--- spec/models/box_spec.rb
+++ spec/models/box_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Box, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {create(:user)}
+
+  it 'has a valid factory' do
+    expect(build(:box, user: user)).to be_valid
+  end
 end
