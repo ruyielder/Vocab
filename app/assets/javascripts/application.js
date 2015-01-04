e0c5865ff7a2db01c600c0cda28b5c0f2cf88b95
ruyielder <ruyielder@gmail.com>
2015-01-04 15:20:49 +0100

    Added angular-rails-templates to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require angular/angular
-//= require angular-route/angular-route
+//= require angular
+//= require angular-route
+//= require angular-rails-templates
 //= require_tree .
